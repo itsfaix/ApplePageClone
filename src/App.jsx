28a3a897caf5d404496747dbf4f068b028ee89f6
Mortenlost <musicaarhe@gmail.com>
2026-03-05 17:56:00 -0600

    3D MacbookModel update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import HeroSection from './components/HeroSection'
+import ProductViewer from './components/ProductViewer'
+import gsap from 'gsap'
+import { ScrollTrigger, SplitText } from 'gsap/ScrollTrigger'
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <HeroSection />
+      <ProductViewer />
     </main>
   )
 }
