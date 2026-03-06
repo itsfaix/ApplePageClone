aa9ce14b1589a80efc5fb2b38efbe05734b1a2eb
Mortenlost <musicaarhe@gmail.com>
2026-03-05 21:11:26 -0600

    Animations

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import NavBar from './components/NavBar'
 import HeroSection from './components/HeroSection'
 import ProductViewer from './components/ProductViewer'
 import gsap from 'gsap'
-import { ScrollTrigger, SplitText } from 'gsap/ScrollTrigger'
+import { ScrollTrigger, SplitText } from 'gsap/all'
 
+gsap.registerPlugin(ScrollTrigger)
 const App = () => {
   return (
     <main>
--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,12 +1,12 @@
 import useMacbookStore from '../store'
 import clsx from 'clsx'
 import { Canvas } from '@react-three/fiber'
-import { Box, OrbitControls} from '@react-three/drei';
-import MacbookModel14 from './models/Macbook-14';
-import MacbookModel16 from './models/Macbook-16';
-import StudioLights from './StudioLights';
+import StudioLights from './three/StudioLights';
+import ModelSwitcher from './three/ModelSwitcher';
+import { useMediaQuery } from 'react-responsive';
 const ProductViewer = () => {
     const {color, scale, setColor, setScale} = useMacbookStore()
+    const isMobile = useMediaQuery( { query: '(max-width: 1024px)' } )
 
   return (
     <section id='product-viewer'>
@@ -44,8 +44,7 @@ const ProductViewer = () => {
         </div>
         <Canvas id="canvas" camera={{position: [0, 2, 5], fov: 50, near: 0.1, far: 100,}}>
             <StudioLights />
-            <MacbookModel14 scale={0.06} position={[0, 0, 0]} />
-            <OrbitControls enableZoom={false}  />
+            <ModelSwitcher scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile}  />
         </Canvas>
         
     </section>
