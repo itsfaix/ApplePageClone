db04388e8d673ba854d604c51d730ff0b45ea44f
Mortenlost <musicaarhe@gmail.com>
2026-03-06 18:25:45 -0600

    Texture updates

--- src/components/models/Macbook-16.jsx
+++ src/components/models/Macbook-16.jsx
@@ -17,8 +17,9 @@ import { noChangeParts } from '../../constants';
 
 export default function MacbookModel16(props) {
   const { nodes, materials,scene } = useGLTF('/models/macbook-16-transformed.glb')
-  const texture = useTexture('/screen.png');
   const { color } = useMacbookStore();
+  const texture = useTexture('/screen.png');
+
 
 
   useEffect(() => {
@@ -50,7 +51,7 @@ export default function MacbookModel16(props) {
       <mesh geometry={nodes.Object_82.geometry} material={materials.gMtYExgrEUqPfln} rotation={[Math.PI / 2, 0, 0]} />
       <mesh geometry={nodes.Object_96.geometry} material={materials.PaletteMaterial003} rotation={[Math.PI / 2, 0, 0]} />
       <mesh geometry={nodes.Object_107.geometry} material={materials.JvMFZolVCdpPqjj} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.Object_123.geometry} material={materials.sfCQkHOWyrsLmor} rotation={[Math.PI / 2, 0, 0]} > 
+      <mesh geometry={nodes.Object_123.geometry} rotation={[Math.PI / 2, 0, 0]} > 
         <meshBasicMaterial map={texture} />
       </mesh>
       <mesh geometry={nodes.Object_127.geometry} material={materials.ZCDwChwkbBfITSW} rotation={[Math.PI / 2, 0, 0]} />
